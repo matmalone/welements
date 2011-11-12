47d4ca961e77c341e80cc840b5fc807f3c964c2f
Mat Malone <matmalone@gmail.com>
2011-11-12 00:07:49 -0800

    converted to test::unit

--- test/test_station_writer.rb
+++ test/test_station_writer.rb
@@ -3,39 +3,39 @@ require File.join(File.dirname(__FILE__), 'test_helper')
 class TestStationWriter < NOAA::TestCase
   XML_DOC = LibXML::XML::Document.file(File.join(File.dirname(__FILE__), 'data', 'stations-abridged.xml'))
 
-  [40.66, 40.77, 40.783].each_with_index do |latitude, i|
-    test "should write latitude for station #{i}" do
-      yaml[i]['latitude'].should == latitude
+  def test_should_write_latitude_for_station
+    [40.66, 40.77, 40.783].each_with_index do |latitude, i|
+      assert_equal yaml[i]['latitude'], latitude
     end
   end
 
-  [-73.78, -73.9, -73.967].each_with_index do |longitude, i|
-    test "should write longitude for station #{i}" do
-      yaml[i]['longitude'].should == longitude
+  def test_should_write_longitude_for_station
+    [-73.78, -73.9, -73.967].each_with_index do |longitude, i|
+      assert_equal yaml[i]['longitude'], longitude
     end
   end
 
-  ['KJFK', 'KLGA', 'KNYC'].each_with_index do |id, i|
-    test "should write id for station #{i}" do
-      yaml[i]['id'].should == id
+  def test_should_write_id_for_station
+    ['KJFK', 'KLGA', 'KNYC'].each_with_index do |id, i|
+      assert_equal yaml[i]['id'], id
     end
   end
 
-  ['New York/John F. Kennedy Intl Airport', 'New York, La Guardia Airport', 'New York City, Central Park'].each_with_index do |name, i|
-    test "should write name for station #{i}" do
-      yaml[i]['name'].should == name
+  def test_should_write_name_for_station
+    ['New York/John F. Kennedy Intl Airport', 'New York, La Guardia Airport', 'New York City, Central Park'].each_with_index do |name, i|
+      assert_equal yaml[i]['name'], name
     end
   end
 
-  (%w(NY) * 3).each_with_index do |state, i|
-    test "should write state for station #{i}" do
-      yaml[i]['state'].should == state
+  def test_should_write_state_for_station
+    (%w(NY) * 3).each_with_index do |state, i|
+      assert_equal yaml[i]['state'], state
     end
   end
 
-  3.times do |i|
-    test "should write XML URL for station #{i}" do
-      yaml[i]['xml_url'].should == "http://weather.gov/xml/current_obs/#{yaml[i]['id']}.xml"
+  def test_should_write_XML_URL_for_station
+    3.times do |i|
+      assert_equal yaml[i]['xml_url'], "http://weather.gov/xml/current_obs/#{yaml[i]['id']}.xml"
     end
   end
 
