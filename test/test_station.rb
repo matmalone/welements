a7b30d1ee4486a48fdc5a0f19756005c8b4acbc3
Mat Malone <matmalone@gmail.com>
2011-11-11 23:56:59 -0800

    converted to test::unit

--- test/test_station.rb
+++ test/test_station.rb
@@ -1,60 +1,60 @@
 require File.join(File.dirname(__FILE__), 'test_helper')
 
 class TestStation < NOAA::TestCase
-  before :all do
+  def setup
     NOAA::Station.stations_file = File.join(File.dirname(__FILE__), 'data', 'stations.yml')
   end
 
-  after :all do
+  def teardown 
     NOAA::Station.stations_file = nil
   end
 
-  test 'should load station by id' do
-    NOAA::Station.find('KNYC').id.should == 'KNYC'
+  def test_should_load_station_by_id
+    assert_equal NOAA::Station.find('KNYC').id, 'KNYC'
   end
 
-  test 'should find closest to coordinates' do
-    NOAA::Station.closest_to(GeoKit::LatLng.new(40.8, -73.96)).id.should == 'KNYC'
+  def test_should_find_closest_to_coordinates
+    assert_equal NOAA::Station.closest_to(GeoKit::LatLng.new(40.8, -73.96)).id, 'KNYC'
   end
 
-  test 'should find closest to lat/lng' do
-    NOAA::Station.closest_to(40.8, -73.96).id.should == 'KNYC'
+  def test_should_find_closest_to_lat_lng
+    assert_equal NOAA::Station.closest_to(40.8, -73.96).id, 'KNYC'
   end
 
-  test 'should find closest to lat/lng passed as array' do
-    NOAA::Station.closest_to([40.8, -73.96]).id.should == 'KNYC'
+  def test_should_find_closest_to_lat_lng_passed_as_array
+    assert_equal NOAA::Station.closest_to([40.8, -73.96]).id, 'KNYC'
   end
 
-  test 'should throw ArgumentError if bad argument passed into #closest_to()' do
-    lambda { NOAA::Station.closest_to('hey') }.should raise_error(ArgumentError)
+  def test_should_throw_ArgumentError_if_bad_argument_passed_into_closest_to
+    assert_raise(ArgumentError){ NOAA::Station.closest_to('hey') }
   end
 
-  test 'should throw ArgumentError if more than two arguments passed into #closest_to()' do
-    lambda { NOAA::Station.closest_to(1, 2, 3) }.should raise_error(ArgumentError)
+  def test_should_throw_ArgumentError_if_more_than_two_arguments_passed_into_closest_to
+    assert_raise(ArgumentError){ NOAA::Station.closest_to(1, 2, 3) }
   end
 
-  test 'should return name' do
-    station.name.should == 'New York City, Central Park'
+  def test_should_return_name
+    assert_equal station.name, 'New York City, Central Park'
   end
 
-  test 'should return state' do
-    station.state.should == 'NY'
+  def test_should_return_state
+    assert_equal station.state, 'NY'
   end
 
-  test 'should return XML URL' do
-    station.xml_url.should == 'http://weather.gov/xml/current_obs/KNYC.xml'
+  def test_should_return_XML_URL
+    assert_equal station.xml_url, 'http://weather.gov/xml/current_obs/KNYC.xml'
   end
 
-  test 'should return latitude' do
-    station.latitude.should == 40.783
+  def test_should_return_latitude
+    assert_equal station.latitude, 40.783
   end
 
-  test 'should return longitude' do
-    station.longitude.should == -73.967
+  def test_should_return_longitude
+    assert_equal station.longitude, -73.967
   end
 
-  test 'should return coordinates' do
-    station.coordinates.should == GeoKit::LatLng.new(40.783, -73.967)
+  def test_should_return_coordinates
+    assert_equal station.coordinates, GeoKit::LatLng.new(40.783, -73.967)
   end
 
   private
