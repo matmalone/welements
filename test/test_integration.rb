c4764116fa2196218e50f5f7450ee2ba3f39e20a
Mat Malone <matmalone@gmail.com>
2012-09-02 12:30:03 -0700

    use assert_operator

--- test/test_integration.rb
+++ test/test_integration.rb
@@ -2,9 +2,9 @@ require File.join(File.dirname(__FILE__), 'test_helper')
 class TestIntegration < Welements::TestCase
   def test_integration
     forecast = Welements.welements(34.5, -120)
-    assert(forecast.probability_of_precipitation.length > 3, "make
+    assert_operator(forecast.probability_of_precipitation.length, :>, 3, "make
 sure we get some number of days of results")
-    assert(forecast.temperature_hourly[0][:value] > 1, "temperature in Santa
+    assert_operator(forecast.temperature_hourly[0][:value], :>, 1, "temperature in Santa
 Barbara should be greater than 1 C")
   end
 end
